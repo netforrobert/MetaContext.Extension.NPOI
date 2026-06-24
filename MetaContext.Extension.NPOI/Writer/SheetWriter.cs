032dc1436c87cdd3eeb7ecbf183667f79f0ced60
robert <tylzlcl@163.com>
2026-06-24 16:41:01 +0800

    默认维护写入起始行

--- MetaContext.Extension.NPOI/Writer/SheetWriter.cs
+++ MetaContext.Extension.NPOI/Writer/SheetWriter.cs
@@ -4,6 +4,7 @@ using System.Linq;
 
 using MetaContext.Extension.NPOI.ColumnIndex;
 using MetaContext.Extension.NPOI.Header;
+using MetaContext.Extension.NPOI.Reader;
 
 using NPOI.POIFS.Crypt.Dsig;
 using NPOI.SS.UserModel;
@@ -16,6 +17,7 @@ internal class SheetWriter : ISheetWriter
 {
     private readonly List<ISheetHeader> _sheetHeaders = new();
     private readonly ISheet _sheet;
+    private int _rowIndex = -1;
 
     public SheetWriter(ISheet sheet)
         => _sheet = sheet;
@@ -53,23 +55,27 @@ internal class SheetWriter : ISheetWriter
         var headerColumns = lastHeader.HeaderTexts.ToArray();
         var startColIndex = lastHeader.StartColIndex;
         PropertyGetterProvider getterProvider = new();
-        int rowIndex = startRowIndex;
-        if (rowIndex == -1)
-            rowIndex = _sheetHeaders.Select(p => p.Rows).Sum();
+
+        _rowIndex = startRowIndex switch
+        {
+            -1 when _rowIndex == -1 => _sheetHeaders.Select(p => p.Rows).Sum(),
+            not -1 => startRowIndex,
+            _ => _rowIndex
+        };
 
         ColumnIndices columnIndices = new(headerColumns, startColIndex);
         foreach (var sourceObject in sourceObjects)
         {
             int rows = rowsSelector?.Invoke(sourceObject) ?? 1;
-            var dataRow = _sheet.GetRow(rowIndex) ?? _sheet.CreateRow(rowIndex);
+            var dataRow = _sheet.GetRow(_rowIndex) ?? _sheet.CreateRow(_rowIndex);
             var rowWriter = new RowSetter(dataRow, columnIndices, rows);
-            IRowsWriter<TSourceObject> rowsWriter = new RowsWriter<TSourceObject>(rowIndex,
+            IRowsWriter<TSourceObject> rowsWriter = new RowsWriter<TSourceObject>(_rowIndex,
                 sourceObject,
                 rowWriter,
                 _sheet);
 
             writerAction(rowsWriter);
-            rowIndex += rows;
+            _rowIndex += rows;
         }
 
         return this;
